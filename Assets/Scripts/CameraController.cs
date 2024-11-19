d3b08af7a43e428638ee9de64658ee662e47e6ce
minh18ndh <minh.nguyendohoang@gmail.com>
2024-11-20 01:33:44 +0700

    Rename script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     [Header("Follow Settings")]
     public Transform target;       // The player (car) to follow
