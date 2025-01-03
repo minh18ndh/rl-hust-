faf6c8c1d4e72ea624e471656eccf64f044c497e
minh18ndh <minh.nguyendohoang@gmail.com>
2025-01-03 23:58:31 +0700

    Add checkpoints

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Collided with: " + other.name);
+
+            HandleCheckpoint();
+        }
+    }
+
+    private void HandleCheckpoint()
+    {
+
+    }
+}
